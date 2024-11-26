a57a85e590099f346589f568f516dd1216ebaacf
Sequoia <sequoiahancock614@gmail.com>
2024-11-25 17:36:17 -0800

    Create player input.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
     private static char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
 
     public static void main(String[] args) {
         initializeBoard();
-        printBoard();
+        while (true) {
+            printBoard();
+            playerMove();
+            if (isWinner()) {
+                printBoard();
+                System.out.println("Player " + currentPlayer + " won!");
+                break;
+            }
+            if (isBoardFull()) {
+                printBoard();
+                System.out.println("The game is a tie!");
+                break;
+            }
+            switchPlayer();
+        }
+    }
+
+    private static void playerMove() {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        while (true) {
+            System.out.print("Player " + currentPlayer + ", enter your move (row and column):");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = currentPlayer;
+                break;
+            }
+            else {
+                System.out.println("Invalid move!");
+            }
+        }
     }
 
     //initialize board with empty spaces
